150e4a52062ff2386b8906a1bcd8a649b3ebcb4c
Reggie Stevens <inzscript@reggies-air.lan>
2022-03-14 00:31:42 -0700

    Add fallback pages

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful'
 import Image from 'next/image'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Skeleton from '../../components/Skeleton'
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -21,7 +22,7 @@ export const getStaticPaths = async () => {
   // get slug paths for all recipes and set fallback if not found
   return {
     paths,
-    fallback: false
+    fallback: true
   }
 }
 
@@ -42,6 +43,8 @@ export async function getStaticProps({ params }) {
 export default function RecipeDetails({ recipe }) {
   console.log(recipe)
   // destructure data
+  if(!recipe) return <Skeleton />
+
   const { featuredImage, title, cookingTime, ingredients, method } = recipe.fields
 
   return (
